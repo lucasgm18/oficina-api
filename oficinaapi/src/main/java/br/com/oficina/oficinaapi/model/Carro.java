a63728e0f3205d17607f1480dec0d2c21b69230d
lucasgm18 <lucasgomesm1808@gmail.com>
2024-05-21 14:55:21 -0400

    getters e setters

--- oficinaapi/src/main/java/br/com/oficina/oficinaapi/model/Carro.java
+++ oficinaapi/src/main/java/br/com/oficina/oficinaapi/model/Carro.java
@@ -14,13 +14,18 @@ import java.util.Objects;
     @Id
     private String vin;
     private String placa;
+    private String modelo;
+    private String marca;
+    private String ano;
 
     public Carro() {
     }
 
-    public Carro(String vin, String placa) {
+    public Carro(String vin, String placa, String modelo, String marca, String Ano) {
         this.vin = vin;
         this.placa = placa;
+        this.modelo = modelo;
+        this.marca = marca;
     }
 
     public String getVin() {
@@ -39,7 +44,31 @@ import java.util.Objects;
         this.placa = placa;
     }
 
-    @Override
+      public String getModelo() {
+          return modelo;
+      }
+
+      public void setModelo(String modelo) {
+          this.modelo = modelo;
+      }
+
+      public String getMarca() {
+          return marca;
+      }
+
+      public void setMarca(String marca) {
+          this.marca = marca;
+      }
+
+      public String getAno() {
+          return ano;
+      }
+
+      public void setAno(String ano) {
+          this.ano = ano;
+      }
+
+      @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -52,11 +81,16 @@ import java.util.Objects;
         return Objects.hashCode(vin);
     }
 
-    @Override
-    public String toString() {
-        return "Carro{" +
-                "vin='" + vin + '\'' +
-                ", placa='" + placa + '\'' +
-                '}';
-    }
-}
+      @Override
+      public String toString() {
+          return "Carro{" +
+                  "vin='" + vin + '\'' +
+                  ", placa='" + placa + '\'' +
+                  ", modelo='" + modelo + '\'' +
+                  ", marca='" + marca + '\'' +
+                  ", ano=" + ano +
+                  '}';
+      }
+
+  }
+
